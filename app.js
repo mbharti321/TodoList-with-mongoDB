36f35494929a48e3926beda76ee287bb0cc53602
mbharti321 <mbharti321@gmail.com>
2021-10-24 00:49:04 +0530

    add insert, read, and delete operation code

--- app.js
+++ app.js
@@ -2,47 +2,91 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
-app.get("/", function(req, res) {
 
-const day = date.getDate();
+mongoose.connect("mongodb://localhost:27017/todolistDb");
 
-  res.render("list", {listTitle: day, newListItems: items});
-
+const itemsSchema = mongoose.Schema({
+  name: String
 });
 
-app.post("/", function(req, res){
+const Item = mongoose.model("Item", itemsSchema);
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+const item1 = new Item({
+  name: "Welcome to my ToDo List"
+});
+const item2 = new Item({
+  name: "Hit + to add new item"
+});
+const item3 = new Item({
+  name: "others"
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function (req, res) {
+  // const day = date.getDate();
+  Item.find({}, function (err, foundItems) {
+    if (err) {
+      console.log(err);
+    } else {
+      // if the list is empty
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems, function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Default data inserted succefully");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: foundItems });
+      }
+
+    }
+  });
 });
 
-app.get("/about", function(req, res){
+app.post("/", function (req, res) {
+
+  const newItem = req.body.newItem;
+  const item = new Item({
+    name: newItem
+  });
+  item.save();
+  res.redirect("/");
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.post("/delete", function (req, res) {
+  // console.log(res.body.checkBox)
+  const checkedItem = req.body.checkBox;
+  Item.deleteOne({ _id: checkedItem }, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Item deleted successfully...");
+      res.redirect("/");
+    };
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server Started!!\nListening to port: http://localhost:3000/")
 });
